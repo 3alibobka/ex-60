b8a4de8a51a783af9e04af7a3f25fc315ccbfe4c
Ivan <3alib0bka@mail.ru>
2022-08-08 16:37:34 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[,,] arr = new int[2,2,2];
+
+int n = 10;
+
+for (int i = 0; i < 2; i++)
+{
+for (int j = 0; j < 2; j++)
+{
+for (int k = 0; k < 2; k++)
+{
+arr[i,j,k] = n;
+Console.WriteLine($"{arr[i,j,k]} ({i}{j}{k})");
+
+n++;
+}
+}
+}
